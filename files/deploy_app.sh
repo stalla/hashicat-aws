1aef4b4841bf3899c7dab38e63d3ab4886d3a044
stalla <satchit_talla@yahoo.com>
2022-06-01 16:41:48 -0600

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to MY VCS RUN app.
+  Welcome to MY VCS RUN2 app.
   <!-- END -->
 
   </div>
