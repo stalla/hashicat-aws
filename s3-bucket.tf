fd29442a825613d58debb16c50ea03bf44e97352
Satchit Talla <satchit_talla@yahoo.com>
2022-06-01 23:35:49 +0000

    Added s3 bucket module - fix1

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/satchit-training/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-bucket_prefix = "SatchitTalla"
+  bucket_prefix = var.prefix
 }
